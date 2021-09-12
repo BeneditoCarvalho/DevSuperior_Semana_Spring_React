cc4ae6ea7c89c9ea00260edb851e5dbf10bb0dd3
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-09-12 13:56:52 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Bem vindo!</h1>
+            <h2>Digite "/dashboard" no final da url para acessar o Dashboard</h2>
+        </div>
+    );
+}
+
+export default Home;
